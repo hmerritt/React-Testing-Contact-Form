2f1b6e4649364ca12171d21453aca2b363709725
Harry Merritt <harry@merritt.es>
2020-02-21 00:55:10 +0000

    Set-up test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import "mutationobserver-shim";
 import App from "./App";
 
 test("renders App without crashing", () => {
-  render(<App />);
+    render(<App />);
 });
